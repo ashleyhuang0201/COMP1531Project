8e2b613e291ba38426e22dc9ecb4923878a1fe8a
z5259842 <z5259842@cse.unsw.edu.au>
2019-10-01 16:15:33 +1000

    skeleton functions

---
+++ server/channel_functions.py
@@ -0,0 +1,40 @@
+# Dummy implementations of channel_functions
+# Implemented by: Ashley Huang
+# Created on: 1/10/2019
+
+import pytest
+
+def channel_invite(token, channel_id, u_id):
+    pass
+
+
+def channel_details(token, channel_id):
+    pass
+
+def channel_messages(token, channel_id, start):
+    pass
+
+def channel_leave(token, channel_id):
+    pass
+
+def channel_join(token, channel_id):
+    pass
+
+
+def channel_addowner(token, channel_id, u_id):
+    pass
+
+def channel_removeowner(token, channel_id, u_id):
+    pass
+
+def channels_list(token):
+    pass
+
+def channels_listall(token):
+    pass
+
+def channels_create(token, name, is_public):
+    pass
+
+
+
