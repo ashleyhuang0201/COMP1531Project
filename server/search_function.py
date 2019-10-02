3ed0d208fc23549e3353d840429f7aa0997763cc
Coen Townson <z5161570@ad.unsw.edu.au>
2019-10-03 01:00:24 +1000

    Made search more specfic to reduce test overlap when other messages will exist

--- server/search_function.py
+++ server/search_function.py
@@ -10,21 +10,21 @@ matches = [];
 
 # Dummy search function and returns
 def search(token, query_str):
-    if (query_str == 'Multichannel'):
+    if (query_str == '93336257 Multichannel search '):
         # Messages 'Multichannel search 2' and 'Multichannel search 1' respectively
         # Ordered this way since I'm assuming search should return most recent 
         # results first
         matches.append(test.messages2[0])
         matches.append(test.messages1[0])
         return matches
-    elif (query_str == 'Singlechannel'):
+    elif (query_str == '93336255 Singlechannel match '):
         # Messages 'Singlechannel search 2' and 'Singlechannel search 1' respectively
         matches.append(test.messages1[2])
         matches.append(test.messages1[3])
         return matches
-    elif (query_str == 'Only'):
+    elif (query_str == '93336256 Only this'):
         # Message 'Only this'
         matches.append(test.messages1[1])
         return matches
     else:
-        return [[]]
+        return []
