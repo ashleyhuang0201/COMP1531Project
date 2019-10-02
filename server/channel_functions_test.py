7d9c3a1848cd85b08819789b517c1c0ae1c0a5e5
z5259842 <z5259842@cse.unsw.edu.au>
2019-10-03 09:21:27 +1000

    named funcs

--- server/channel_functions_test.py
+++ server/channel_functions_test.py
@@ -4,3 +4,33 @@
 
 import pytest
 import channel_functions as func
+
+def test_channel_invite():
+    pass
+
+def test_channel_details():
+    pass
+
+def test_channel_messages():
+    pass
+
+def test_channel_leave():
+    pass
+
+def test_channel_join():
+    pass
+
+def test_channel_addowner():
+    pass
+
+def test_channel_removeowner():
+    pass
+
+def test_channels_list():
+    pass
+
+def test_channels_listall():
+    pass
+
+def test_channels_create():
+    pass
