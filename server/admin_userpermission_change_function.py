cbf84d4f04828ab02267cd6f3d058cce48506608
Coen Townson <z5161570@ad.unsw.edu.au>
2019-10-03 15:53:27 +1000

    Created files2

--- server/admin_userpermission_change_function.py
+++ server/admin_userpermission_change_function.py
@@ -1,3 +1,6 @@
 # Dummy implementation for permission change function
 # Created by: Coen Townson
 # Created on: 3/10/2019
+
+def admin_userpermission_change(token, u_id, permission_id):
+    pass
--- server/admin_userpermission_change_function_test.py
+++ server/admin_userpermission_change_function_test.py
@@ -1,3 +1,6 @@
 # Tests for the permission change function function
 # Created by: Coen Townson
 # Created on: 3/10/2019
+
+import pytest
+from admin_userpermission_change_function import admin_userpermission_change
