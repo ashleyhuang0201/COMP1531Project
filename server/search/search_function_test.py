fc7305af4f6c3faf2f68466571ce6378081ff6d5
Coen Townson <c.townson@student.unsw.edu.au>
2019-10-04 14:55:13 +1000

    Update search_function_test.py

--- server/search/search_function_test.py
+++ server/search/search_function_test.py
@@ -3,7 +3,7 @@
 # Created on: 2/10/2019
 
 import pytest
-from server.search.search_function import search
+from server.search import search_function as search
 from server.message.message_functions import message_send
 from server.auth.auth_functions import auth_register
 from server.channel.channel_functions import channels_create, channel_messages
@@ -29,23 +29,23 @@ messages2 = channel_messages(user['token'], channel2, 0)
 
 # Search for nothing and get nothing
 def test_search_none():
-    assert search(user['token'], '') == {'messages': []}
+    assert search.search(user['token'], '') == {'messages': []}
 
 # Search for message that shouldn't exist and get nothing
 def test_search_empty():
-    assert search(user['token'], '63331448511 this is a bad search and should not be filled') == {'messages': []}
+    assert search.search(user['token'], '63331448511 this is a bad search and should not be filled') == {'messages': []}
 
 # Search and get 1 message back
 def test_search_one():
-    assert search(user['token'], '93336256 Only this') == {'messages': messages1[1]}
+    assert search.search(user['token'], '93336256 Only this') == {'messages': messages1[1]}
 
 # Search and get 2 messages from the same channel back
 def test_search_single_channel():
-    assert search(user['token'], '93336255 Singlechannel match ') == {'messages': [messages1[2], messages1[3]]}
+    assert search.search(user['token'], '93336255 Singlechannel match ') == {'messages': [messages1[2], messages1[3]]}
 
 # Search and get 2 messages from different channels back
 def test_search_multi_channel():
-    assert search(user['token'], '93336257 Multichannel search ') == {'messages': [messages2[0], messages1[0]]}
+    assert search.search(user['token'], '93336257 Multichannel search ') == {'messages': [messages2[0], messages1[0]]}
 
 
 ##### SALLY AND BOB WILL
