4ad4f3d89321b0ca552412722b39d4f462231fc6
Coen Townson <c.townson@student.unsw.edu.au>
2019-10-05 15:59:32 +1000

    Update tests_for_kevin.sh

--- tests_for_kevin.sh
+++ tests_for_kevin.sh
@@ -3,14 +3,14 @@ for dir in `ls server`
 do
     for file in `ls server/$dir`
     do
-        if [[ "$file" == "__pycache__" ]]
-        then
-            `rm -r server/$dir/$file`
-        fi
         to_run=`echo "$file" | egrep -o "[^.]+_test\.py"`
         if [[ "$to_run" != "" ]]
         then
             pytest "server/$dir/$to_run"
+        fi
+        if [[ "$file" == "__pycache__" ]]
+        then
+            `rm -r server/$dir/$file`
         fi
     done
 done
