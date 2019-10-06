367e09f46b876a4508fe2d9b7f0b6c19b6475721
Coen Townson <z5161570@unsw.edu.au>
2019-10-06 15:54:35 +1100

    Fixed standup

--- server/standup/standup_functions_test.py
+++ server/standup/standup_functions_test.py
@@ -6,81 +6,75 @@ from server.channel import channel_functions as channel_func
 
 def test_standup_start():
     # A valid token and channel successfully starts a standup - Owner
-    user = auth.auth_register("validcorrect@g.com", "valid_password", \
-         "valid_correct_first_name", "valid_correct_last_name")
-    channel = channel_func.channels_create(user["token"], "Group Name", True)
-    assert standup.standup_start(user["token"], channel["channel_id"]) \
-        == {"time": 900}
-    # A valid token and channel successfully starts a standup - User
     owner = auth.auth_register("validcorrect@g.com", "valid_password", \
          "valid_correct_first_name", "valid_correct_last_name")
-    channel = channel_func.channels_create(owner["token"], "Group Name", True)
-    user = auth.auth_register("validcorrect1@g.com", "valid_password", \
+    
+    channel1 = channel_func.channels_create(owner["token"], "Owner", True)
+    
+    assert standup.standup_start(owner["token"], channel1["channel_id"]) \
+        == {"time": get_standup_end()}
+
+    # A valid token and channel successfully starts a standup - User
+    user = auth.auth_register("validcorrect@g.com", "valid_password", \
          "valid_correct_first_name", "valid_correct_last_name")
-    assert standup.standup_start(user["token"], channel["channel_id"]) \
-        == {"time": 900}
+    
+    channel2 = channel_func.channels_create(user["token"], "User", True)
+    
+    assert standup.standup_start(user["token"], channel2["channel_id"]) \
+        == {"time": get_standup_end()}
+
     # Channel id given does not exist
     with pytest.raises(ValueError, match = "Channel Does Not Exist"):
-        user = auth.auth_register("validcorrect@g.com", "valid_password", \
-            "valid_correct_first_name", "valid_correct_last_name")
         standup.standup_start(user["token"], 21512512521512)
+
     # User has not joined the channel
     with pytest.raises(AccessError, match = "Cannot Access Channel"):
-        owner = auth.auth_register("validcorrect@g.com", "valid_password", \
+        user2 = auth.auth_register("validcorrect2@g.com", "valid_password", \
             "valid_correct_first_name", "valid_correct_last_name")
-        channel = channel_func.channels_create(owner["token"], "Group Name", True)
-        user = auth.auth_register("validcorrect1@g.com", "valid_password", \
-            "valid_correct_first_name", "valid_correct_last_name")
-        time = standup.standup_start(user["token"], channel["channel_id"])
+        
+        standup.standup_start(user2["token"], channel1["channel_id"])
 
 def test_standup_send():
     # A message is buffered in the standup queue - Owner
-    user = auth.auth_register("validcorrect@g.com", "valid_password", \
-         "valid_correct_first_name", "valid_correct_last_name")
-    channel = channel_func.channels_create(user["token"], "Group Name", True)
-    time = standup.standup_start(user["token"], channel["channel_id"])
-    assert standup.standup_send(user["token"], channel["channel_id"], \
-        "correct_and_valid_message") == {}
-    # A message is buffered in the standup queue - Member
     owner = auth.auth_register("validcorrect@g.com", "valid_password", \
          "valid_correct_first_name", "valid_correct_last_name")
-    channel = channel_func.channels_create(owner["token"], "Group Name", True)
+    
+    channel = channel_func.channels_create(owner["token"], "Owner", True)
+    
+    standup.standup_start(owner["token"], channel["channel_id"])
+    
+    assert standup.standup_send(owner["token"], channel["channel_id"], \
+        "correct_and_valid_message") == {}
+
+    # A message is buffered in the standup queue - Member
     user = auth.auth_register("validcorrect1@g.com", "valid_password", \
-        "valid_correct_first_name", "valid_correct_last_name")
-    channel_func.channel_join(user["token"], channel["channel_id"])
-    time = standup.standup_start(owner["token"], channel["channel_id"])
+         "valid_correct_first_name", "valid_correct_last_name")
+    
     assert standup.standup_send(user["token"], channel["channel_id"], \
         "correct_and_valid_message") == {}
+
     # Channel given does not exist
     with pytest.raises(ValueError, match = "Channel Does Not Exist"):
-        user = auth.auth_register("validcorrect@g.com", "valid_password", \
-            "valid_correct_first_name", "valid_correct_last_name")
         standup.standup_send(user["token"], 523523523, \
             "correct_and_valid_message")
+
     # The message sent was greater than max message length
     with pytest.raises(ValueError, match = "Message Too Long"):
-        user = auth.auth_register("validcorrect@g.com", "valid_password", \
-            "valid_correct_first_name", "valid_correct_last_name")
-        channel = channel_func.channels_create(user["token"], "Group Name", True)
-        time = standup.standup_start(user["token"], channel["channel_id"])
         standup.standup_send("valid_token", \
              "correct_and_valid_channel_with_standup", string_creator(1001))
+
     # User has not joined channel
     with pytest.raises(AccessError, match = "Cannot Access Channel"):
-        owner = auth.auth_register("validcorrect@g.com", "valid_password", \
+        user2 = auth.auth_register("validcorrect2@g.com", "valid_password", \
             "valid_correct_first_name", "valid_correct_last_name")
-        channel = channel_func.channels_create(owner["token"], "Group Name", True)
-        user = auth.auth_register("validcorrect1@g.com", "valid_password", \
-            "valid_correct_first_name", "valid_correct_last_name")
-        time = standup.standup_start(owner["token"], channel["channel_id"])
-        standup.standup_send(user["token"], channel["channel_id"], \
+        
+        standup.standup_send(user2["token"], channel["channel_id"], \
                   "correct_and_valid_message")
+
     # Channel is not currently in standup mode
     with pytest.raises(AccessError, match = "Not Currently In Standup"):
-        user = auth.auth_register("validcorrect@g.com", "valid_password", \
-            "valid_correct_first_name", "valid_correct_last_name")
-        channel = channel_func.channels_create(user["token"], "Group Name", True)
-        standup.standup_send("valid_token", "correct_and_valid_channel", \
+        channel2 = channel_func.channels_create(user["token"], "Group", True)
+        standup.standup_send(user["token"], channel2["channel_id"], \
              "correct_and_valid_message")
 
 # Creates a variable length string
@@ -89,3 +83,7 @@ def string_creator(length):
     for i in range(length):
         string += "a"
     return string
+
+# returns expected end time of standup (15 mins in future)
+def get_standup_end():
+    return 900
