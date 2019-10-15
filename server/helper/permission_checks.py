12a362eec233ab47d38fa51ecb7ecd04c709602f
Kevin <kevin.chu1@student.unsw.edu.au>
2019-10-15 18:25:07 +1100

    Added to helper functions

--- server/helper/permission_checks.py
+++ server/helper/permission_checks.py
@@ -1,8 +1,10 @@
+# Checks if a user is an admin
 def token_is_admin(token):
     if token  == "admin_token":
         return True
     return False
 
+# Checks if a user is an owner
 def token_is_owner(token):
     if token  == "owner_token":
         return True
--- server/helper/valid_checks.py
+++ server/helper/valid_checks.py
@@ -1,9 +1,11 @@
+import re
+
 # Checks if an email is a valid email
-def valid_email(email):  
-    if email == "test1@gmail.com" or email == "test2@gmail.com":
-        return True
-    else:
-        return False
+def valid_email(email):
+	if re.search('^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$', email):
+		return True
+	else: 
+		return False
 
 # Checks if a user_id is valid
 def valid_user_id(u_id):
