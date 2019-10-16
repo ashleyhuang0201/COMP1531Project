8521e37b80002e50d527a6bcc4d03befbd03f141
Coen Townson <z5161570@unsw.edu.au>
2019-10-16 23:57:00 +1100

    Undoing coen silly subdirs

--- server/helper/permission_checks.py
+++
@@ -1,9 +0,0 @@
-def token_is_admin(token):
-    if token  == "admin_token":
-        return True
-    return False
-
-def token_is_owner(token):
-    if token  == "owner_token":
-        return True
-    return False
