8c7f3853e1153d6f8d4931bcf4007ed212b6a739
Coen Townson <coen.townson21@gmail.com>
2019-10-18 17:50:50 +1100

    Added user class to hopefully make it easier

--- server/global_var.py
+++ server/global_var.py
@@ -1,6 +1,14 @@
 #Single global variable containing nested dictionaries/lists
 data = {
-    "users": [{"u_id", "password", "email", "name_first", \
-                 "name_last", "handle"}],
+    "users": [],
     "tokens": [],
 }
+
+class User:
+    def __init__(self, u_id, email, password, name_first, name_last):
+        self.u_id = u_id
+        self.email = email
+        self.password = password
+        self.name_first = name_first
+        self.name_last = name_last
+        self.handle = f"{name_first}{name_last}"
