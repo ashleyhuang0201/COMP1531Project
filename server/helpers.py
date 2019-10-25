1be3c49fb9fdf29b8fa348e0871e2e8edbbe3e98
Kevin Chu <kevin.chu1@student.unsw.edu.au>
2019-10-25 11:38:07 +1100

    Finished helpers

--- server/helpers.py
+++ server/helpers.py
@@ -1,8 +1,18 @@
 '''
-Helper functions that check validity of user data
+Helper functions
+Checks user validity
+Gets data
 '''
 import re
 import server.global_var as global_var
+
+# Decodes a token
+def decode_token(token):
+    return jwt.decode(token, SECRET, algorithms=['HS256'])
+
+# Encodes user id
+def encode_token_for_u_id(u_id):
+    jwt.encode({"u_id": u_id}, SECRET, algorithm=['HS256'])
 
 def token_is_admin(token):
     """
@@ -27,7 +37,7 @@ def token_is_owner(token):
     if not valid_token(token):
         raise ValueError("Invalid token")
 
-    # Checking if token is an admin
+    # Checking if token is an owner
     user = get_user_by_token(token)
     if user.permission == 1:
         return True
@@ -45,32 +55,46 @@ def valid_email(email):
         return False
 
 def valid_name(name):
-    ''' Checks if a name is valid to be registered '''
-    if len(name) >= 1 and len(name) <= 50:
+    '''
+    Checking if a name is valid
+    '''
+    if len(name) > 0 and len(name) <= 50:
         return True
     else:
         return False
 
 def valid_user_id(u_id):
-    ''' Checks if a user_id is valid '''
-    if u_id == "valid_u_id":
-        return True
+    '''
+    Checking if a user_id is valid
+    '''
+    for user in global_var.data["users"]:
+        if user.u_id == u_id:
+            return True
     return False
 
 def valid_token(token):
-    ''' Checks if a user_id is valid '''
-    if token == "valid_token":
+    '''
+    Checks if a user token is valid
+    '''
+    for token in global_var.data["token"]:
         return True
     return False
 
 def valid_permission_id(permission_id):
+    """
+    Checks if a permission is valid
+    """
     if permission_id == 1:
+        # Owner
         return True
     elif permission_id == 2:
+        # Admin
         return True
     elif permission_id == 3:
+        # User
         return True
     else:
+        # Invalid permission_id
         return False
 
 def get_user_by_u_id(u_id):
@@ -81,23 +105,33 @@ def get_user_by_u_id(u_id):
     if not valid_user_id(u_id):
         raise ValueError("Invalid User ID")
 
+    # Finding user object using u_id
     for user in global_var.data["users"]:
         if user.u_id == u_id:
             return user
 
 def get_user_by_token(token):
     """
-    Returns user class according to their token
+    Returns user object according to their token
     """
     # Checking validity of token
     if not valid_token(token):
         raise ValueError("Invalid token")
 
-    # Finding user_id from token
-    # To do
-    # Reverse token
-    """
-    for user in global_var.data["users"]:
+    # Decoding token
+    u_id = jwt.decode(token, SECRET, algorithms=['HS256'])
+    
+    # Finding user_id
+    for user in global_var.data["user"]:
         if user.u_id == u_id:
             return user
+
+def get_user_token_by_u_id(u_id):
     """
+    Returns user token according to their token
+    """
+    # Checking validity of u_id
+    if not valid_user_id(u_id):
+        raise ValueError("Invalid u_id")
+    
+    return encode_token_for_u_id(u_id)
