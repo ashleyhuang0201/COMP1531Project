ca6532fe47b49b3c6dd51f5919d9ba3ace8e4fa9
Kevin Chu <kevin.chu1@student.unsw.edu.au>
2019-10-25 11:18:12 +1100

    Fixing

--- server/admin_userpermission_change_function_test.py
+++ server/admin_userpermission_change_function_test.py
@@ -8,7 +8,7 @@ to change permission to any three states)
 import pytest
 from server.admin.admin_userpermission_change_function import \
     admin_userpermission_change
-from server.helper.helper import valid_user_id, token_is_admin, token_is_owner, get_user_by_token
+from server.helper.helper import valid_user_id, token_is_admin, token_is_owner, get_user_token_by_u_id
 from server.auth.auth_functions import auth_register
 
 # Invalid token
@@ -27,7 +27,7 @@ def test_invalid_user_id():
         user = auth_register("test@gmail.com", "pass123", "Rayden", "Smith")
         user.permission = 1
         
-        admin_userpermission_change(get_user_by_token(u_id), -1, 2)
+        admin_userpermission_change(get_user_token_by_u_id(u_id), -1, 2)
 
 # Invalid permission
 def test_invalid_permission():
@@ -36,7 +36,7 @@ def test_invalid_permission():
         user = auth_register("test@gmail.com", "pass123", "Rayden", "Smith")
         user.permission = 1
 
-        admin_userpermission_change(get_user_by_token(u_id), user.u_id, -1)
+        admin_userpermission_change(get_user_token_by_u_id(u_id), user.u_id, -1)
 
 # Invalid user
 def test_user_member():
@@ -44,7 +44,7 @@ def test_user_member():
         # Creating a user who cannot change permissions
         user = auth_register("test@gmail.com", "pass123", "Rayden", "Smith")
         
-        admin_userpermission_change(get_user_by_token(u_id), user.u_id, 1)
+        admin_userpermission_change(get_user_token_by_u_id(u_id), user.u_id, 1)
 
 # Successful permission change by admin
 def test_user_admin():
@@ -64,15 +64,15 @@ def test_user_admin():
         admin2.permission = 1
         
         # Admin changing permission of member
-        admin_userpermission_change(get_user_by_token(admin1.u_id), user.u_id, 1)
+        admin_userpermission_change(get_user_token_by_u_id(admin1.u_id), user.u_id, 1)
         assert user.permission == 1
         
         # Admin changing permission of another admin
-        admin_userpermission_change(get_user_by_token(admin1.u_id), admin2.u_id, 1)
+        admin_userpermission_change(get_user_token_by_u_id(admin1.u_id), admin2.u_id, 1)
         assert admin2.permission == 1
         
         # Admin changing permission of owner
-        admin_userpermission_change(get_user_by_token(admin1.u_id), owner.u_id, 2)
+        admin_userpermission_change(get_user_token_by_u_id(admin1.u_id), owner.u_id, 2)
         assert owner.permission == 2
         
 # Successful permission change by owner
@@ -93,15 +93,15 @@ def test_user_owner():
         owner2.permission = 1
         
         # Owner changing permission of member
-        admin_userpermission_change(get_user_by_token(owner1.u_id), user.u_id, 1)
+        admin_userpermission_change(get_user_token_by_u_id(owner1.u_id), user.u_id, 1)
         assert user.permission == 1
         
         # Owner changing permission of another admin
-        admin_userpermission_change(get_user_by_token(owner1.u_id), admin.u_id, 1)
+        admin_userpermission_change(get_user_token_by_u_id(owner1.u_id), admin.u_id, 1)
         assert admin.permission == 1
         
         # Owner changing permission of owner
-        admin_userpermission_change(get_user_by_token(owner1.u_id), owner2.u_id, 2)
+        admin_userpermission_change(get_user_token_by_u_id(owner1.u_id), owner2.u_id, 2)
         assert owner2.permission == 2
 
 # Ability to change permission to user, owner and admin
@@ -114,10 +114,10 @@ def test_user_permission_possible_permissions:
         user = auth_register("member@gmail.com", "pass123", "Rayden", "Smith")
         
         # Changing normal member to admin
-        admin_userpermission_change(get_user_by_token(owner1.u_id), owner.u_id, 2)
+        admin_userpermission_change(get_user_token_by_u_id(owner1.u_id), owner.u_id, 2)
         
         # Changing admin to owner
-        admin_userpermission_change(get_user_by_token(owner1.u_id), owner.u_id, 1)
+        admin_userpermission_change(get_user_token_by_u_id(owner1.u_id), owner.u_id, 1)
         
         # Changing owner to member
-        admin_userpermission_change(get_user_by_token(owner1.u_id), owner.u_id, 3)
+        admin_userpermission_change(get_user_token_by_u_id(owner1.u_id), owner.u_id, 3)
