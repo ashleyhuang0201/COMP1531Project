d30d16a781d5cad9cca0b4066b330f4975956f7c
Coen Townson <coen.townson21@gmail.com>
2019-10-26 17:08:08 +1100

    Fixed small bugs

--- server/admin_userpermission_change_function.py
+++ server/admin_userpermission_change_function.py
@@ -3,7 +3,7 @@
 # Created on: 3/10/2019
 
 from server.Error import AccessError
-from server.helpers import valid_user_id, valid_token, token_is_admin, token_is_owner
+import server.helpers as helper
 
 def admin_userpermission_change(token, u_id, permission_id):
     """
@@ -11,16 +11,16 @@ def admin_userpermission_change(token, u_id, permission_id):
     described by permission_id
     """
     # Checking validity of permission change request
-    if not valid_token(token):
+    if not helper.valid_token(token):
         raise AccessError('Invalid token')
-    if not valid_user_id(u_id):
+    if not helper.valid_user_id(u_id):
         raise ValueError('User ID is invalid')
-    if not valid_permission_id(permission_id):
+    if not helper.valid_permission_id(permission_id):
         raise ValueError('Permission ID is invalid')
-    if not token_is_admin(token) or not token_is_owner(token):
+    if not helper.token_is_admin(token) or not helper.token_is_owner(token):
         raise AccessError('Current user is not an admin or owner')
     
-    user = get_user_by_u_id(u_id)
+    user = helper.get_user_by_u_id(u_id)
     user.change_permissions(permission_id)
 
     return {}
--- server/admin_userpermission_change_function_test.py
+++ server/admin_userpermission_change_function_test.py
@@ -6,10 +6,10 @@ to change permission to any three states)
 """
 
 import pytest
-from server.admin.admin_userpermission_change_function import \
+from server.admin_userpermission_change_function import \
     admin_userpermission_change
-from server.helper.helper import valid_user_id, token_is_admin, token_is_owner, get_user_token_by_u_id
-from server.auth.auth_functions import auth_register
+from server.helpers import valid_user_id, token_is_admin, token_is_owner, get_user_token_by_u_id
+from server.auth_functions import auth_register
 
 # Invalid token
 def test_invalid_token():
@@ -27,7 +27,7 @@ def test_invalid_user_id():
         user = auth_register("test@gmail.com", "pass123", "Rayden", "Smith")
         user.permission = 1
         
-        admin_userpermission_change(get_user_token_by_u_id(u_id), -1, 2)
+        admin_userpermission_change(get_user_token_by_u_id(user.u_id), -1, 2)
 
 # Invalid permission
 def test_invalid_permission():
@@ -36,7 +36,7 @@ def test_invalid_permission():
         user = auth_register("test@gmail.com", "pass123", "Rayden", "Smith")
         user.permission = 1
 
-        admin_userpermission_change(get_user_token_by_u_id(u_id), user.u_id, -1)
+        admin_userpermission_change(get_user_token_by_u_id(user.u_id), user.u_id, -1)
 
 # Invalid user
 def test_user_member():
@@ -44,80 +44,80 @@ def test_user_member():
         # Creating a user who cannot change permissions
         user = auth_register("test@gmail.com", "pass123", "Rayden", "Smith")
         
-        admin_userpermission_change(get_user_token_by_u_id(u_id), user.u_id, 1)
+        admin_userpermission_change(get_user_token_by_u_id(user.u_id), user.u_id, 1)
 
 # Successful permission change by admin
 def test_user_admin():
-        # Creating a valid admin who can change permissions
-        admin1 = auth_register("admin1@gmail.com", "pass123", "Rayden", "Smith")
-        admin1.permission = 2
-        
-        # Creating normal member
-        user = auth_register("member@gmail.com", "pass123", "Rayden", "Smith")
-        
-        # Creating another admin
-        admin2 = auth_register("admin2@gmail.com", "pass123", "Rayden", "Smith")
-        admin2.permission = 2
-        
-        # Creating another owner
-        owner = auth_register("owner@gmail.com", "pass123", "Rayden", "Smith")
-        admin2.permission = 1
-        
-        # Admin changing permission of member
-        admin_userpermission_change(get_user_token_by_u_id(admin1.u_id), user.u_id, 1)
-        assert user.permission == 1
-        
-        # Admin changing permission of another admin
-        admin_userpermission_change(get_user_token_by_u_id(admin1.u_id), admin2.u_id, 1)
-        assert admin2.permission == 1
-        
-        # Admin changing permission of owner
-        admin_userpermission_change(get_user_token_by_u_id(admin1.u_id), owner.u_id, 2)
-        assert owner.permission == 2
+    # Creating a valid admin who can change permissions
+    admin1 = auth_register("admin1@gmail.com", "pass123", "Rayden", "Smith")
+    admin1.permission = 2
+    
+    # Creating normal member
+    user = auth_register("member@gmail.com", "pass123", "Rayden", "Smith")
+    
+    # Creating another admin
+    admin2 = auth_register("admin2@gmail.com", "pass123", "Rayden", "Smith")
+    admin2.permission = 2
+    
+    # Creating another owner
+    owner = auth_register("owner@gmail.com", "pass123", "Rayden", "Smith")
+    admin2.permission = 1
+    
+    # Admin changing permission of member
+    admin_userpermission_change(get_user_token_by_u_id(admin1.u_id), user.u_id, 1)
+    assert user.permission == 1
+    
+    # Admin changing permission of another admin
+    admin_userpermission_change(get_user_token_by_u_id(admin1.u_id), admin2.u_id, 1)
+    assert admin2.permission == 1
+    
+    # Admin changing permission of owner
+    admin_userpermission_change(get_user_token_by_u_id(admin1.u_id), owner.u_id, 2)
+    assert owner.permission == 2
         
 # Successful permission change by owner
 def test_user_owner():
-        # Creating a valid user who can change permissions
-        owner1 = auth_register("owner1@gmail.com", "pass123", "Rayden", "Smith")
-        owner1.permission = 1
-        
-        # Creating normal member
-        user = auth_register("member@gmail.com", "pass123", "Rayden", "Smith")
-        
-        # Creating admin
-        admin = auth_register("admin@gmail.com", "pass123", "Rayden", "Smith")
-        admin.permission = 2
-        
-        # Creating another owner
-        owner2 = auth_register("owner2@gmail.com", "pass123", "Rayden", "Smith")
-        owner2.permission = 1
-        
-        # Owner changing permission of member
-        admin_userpermission_change(get_user_token_by_u_id(owner1.u_id), user.u_id, 1)
-        assert user.permission == 1
-        
-        # Owner changing permission of another admin
-        admin_userpermission_change(get_user_token_by_u_id(owner1.u_id), admin.u_id, 1)
-        assert admin.permission == 1
-        
-        # Owner changing permission of owner
-        admin_userpermission_change(get_user_token_by_u_id(owner1.u_id), owner2.u_id, 2)
-        assert owner2.permission == 2
+    # Creating a valid user who can change permissions
+    owner1 = auth_register("owner1@gmail.com", "pass123", "Rayden", "Smith")
+    owner1.permission = 1
+    
+    # Creating normal member
+    user = auth_register("member@gmail.com", "pass123", "Rayden", "Smith")
+    
+    # Creating admin
+    admin = auth_register("admin@gmail.com", "pass123", "Rayden", "Smith")
+    admin.permission = 2
+    
+    # Creating another owner
+    owner2 = auth_register("owner2@gmail.com", "pass123", "Rayden", "Smith")
+    owner2.permission = 1
+    
+    # Owner changing permission of member
+    admin_userpermission_change(get_user_token_by_u_id(owner1.u_id), user.u_id, 1)
+    assert user.permission == 1
+    
+    # Owner changing permission of another admin
+    admin_userpermission_change(get_user_token_by_u_id(owner1.u_id), admin.u_id, 1)
+    assert admin.permission == 1
+    
+    # Owner changing permission of owner
+    admin_userpermission_change(get_user_token_by_u_id(owner1.u_id), owner2.u_id, 2)
+    assert owner2.permission == 2
 
 # Ability to change permission to user, owner and admin
-def test_user_permission_possible_permissions:
-        # Creating a valid user who can change permissions
-        owner = auth_register("owner@gmail.com", "pass123", "Rayden", "Smith")
-        owner.permission = 1
-        
-        # Creating normal member
-        user = auth_register("member@gmail.com", "pass123", "Rayden", "Smith")
-        
-        # Changing normal member to admin
-        admin_userpermission_change(get_user_token_by_u_id(owner1.u_id), owner.u_id, 2)
-        
-        # Changing admin to owner
-        admin_userpermission_change(get_user_token_by_u_id(owner1.u_id), owner.u_id, 1)
-        
-        # Changing owner to member
-        admin_userpermission_change(get_user_token_by_u_id(owner1.u_id), owner.u_id, 3)
+def test_user_permission_possible_permissions():
+    # Creating a valid user who can change permissions
+    owner = auth_register("owner@gmail.com", "pass123", "Rayden", "Smith")
+    owner.permission = 1
+    
+    # Creating normal member
+    user = auth_register("member@gmail.com", "pass123", "Rayden", "Smith")
+    
+    # Changing normal member to admin
+    admin_userpermission_change(get_user_token_by_u_id(owner.u_id), owner.u_id, 2)
+    
+    # Changing admin to owner
+    admin_userpermission_change(get_user_token_by_u_id(owner.u_id), owner.u_id, 1)
+    
+    # Changing owner to member
+    admin_userpermission_change(get_user_token_by_u_id(owner.u_id), owner.u_id, 3)
