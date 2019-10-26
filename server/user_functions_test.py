2df3ef1731a9470e31dd1eb22fac0bba495fa367
z5259842 <z5259842@cse.unsw.edu.au>
2019-10-27 00:40:21 +1100

    trying to get a 10

--- server/user_functions_test.py
+++ server/user_functions_test.py
@@ -5,20 +5,18 @@ Team: You_Things_Can_Choose
 
 import pytest
 from server import user_functions as funcs
-from server import auth_functions 
-
+from server import auth_functions
 from server.Error import AccessError
 import server.global_var as global_var
 import jwt
 
-
 def test_user_profile():
-    
+
     # Initialisation
     global_var.initialise_all()
     assert global_var.data["users"] == []
 
-    # Creating a user 
+    # Creating a user
     user = auth_functions.auth_register("test@gmail.com", "pass123", \
          "Raydon", "Smith")
 
@@ -29,23 +27,23 @@ def test_user_profile():
 
     # A valid user_id is provided, user details are returned
     assert funcs.user_profile(token, u_id) ==  \
-         {"email":'test@gmail.com', "name_first":'Raydon',"name_last":'Smith', \
+         {"email":'test@gmail.com', "name_first":'Raydon', "name_last":'Smith',\
               "handle_str":'raydonsmith'}
 
     # A exception occurs when the user_id is invalid
-    with pytest.raises(ValueError, match = "Invalid User ID"):
+    with pytest.raises(ValueError, match="Invalid User ID"):
         funcs.user_profile(token, 99)
 
 
 def test_profile_setname():
-    
+
     # Initialisation
     global_var.initialise_all()
     assert global_var.data["users"] == []
 
     # Creating user
     user = auth_functions.auth_register("test@gmail.com", "pass123", \
-         "Rayden", "Smith")
+        "Rayden", "Smith")
 
     assert global_var.data["users"] != []
 
@@ -60,7 +58,7 @@ def test_profile_setname():
 
     # Checking if first and last name have been updated successfully
     assert funcs.user_profile(token, u_id) ==  \
-         {"email":'test@gmail.com', "name_first":'Hello',"name_last":'World', \
+         {"email":'test@gmail.com', "name_first":'Hello', "name_last":'World', \
               "handle_str":'raydensmith'}
 
 
@@ -69,24 +67,24 @@ def test_profile_setname():
          create_50_string()) == {}
 
     # First name too long
-    with pytest.raises(ValueError, match = "Name too long"):
-        funcs.user_profile_setname(token, create_50_string() + "a", "Smith" )
+    with pytest.raises(ValueError, match="Name too long"):
+        funcs.user_profile_setname(token, create_50_string() + "a", "Smith")
 
     # Lasts name too long
-    with pytest.raises(ValueError, match = "Name too long"):
+    with pytest.raises(ValueError, match="Name too long"):
         funcs.user_profile_setname(token, "Raydon", create_50_string() + "a")
 
-      
+
 def test_profile_setemail():
-   
+
     # Initialisation
 
     global_var.initialise_all()
     assert global_var.data["users"] == []
 
     # Creating user
-    user = auth_functions.auth_register("test@gmail.com", "pass123", \
-         "Rayden", "Smith")
+    user = auth_functions.auth_register("test@gmail.com", "pass123",\
+        "Rayden", "Smith")
 
     assert global_var.data["users"] != []
 
@@ -97,24 +95,24 @@ def test_profile_setemail():
     assert funcs.user_profile_setemail(token, "test1@gmail.com") == {}
 
     # Updating the email of the user
-    funcs.user_profile_setemail(token, "new@gmail.com") 
+    funcs.user_profile_setemail(token, "new@gmail.com")
 
     # Checking if the user's email has been updated successfully
     assert funcs.user_profile(token, u_id) ==  \
-         {"email":'new@gmail.com', "name_first":'Rayden',"name_last":'Smith', \
+         {"email":'new@gmail.com', "name_first":'Rayden', "name_last":'Smith', \
               "handle_str":'raydensmith'}
 
     # A invalid email is given
-    with pytest.raises(ValueError, match = "Invalid email"):
+    with pytest.raises(ValueError, match="Invalid email"):
         funcs.user_profile_setemail(token, "invalidEmail")
 
     # A email already in use is given
-    with pytest.raises(ValueError, match = "Email already in use"):
+    with pytest.raises(ValueError, match="Email already in use"):
         funcs.user_profile_setemail(token, "test2@gmail.com")
 
 
 def test_profile_sethandle():
-    
+
     # Initialisation
     global_var.initialise_all()
     assert global_var.data["users"] == []
@@ -132,20 +130,20 @@ def test_profile_sethandle():
     assert funcs.user_profile_sethandle(token, "a handle") == {}
 
     # Updating the handle of the user
-    funcs.user_profile_sethandle(token, "new handle") 
+    funcs.user_profile_sethandle(token, "new handle")
 
     # Checking if the user's handle has been updated successfully
     assert funcs.user_profile(token, u_id) ==  \
-         {"email":'test@gmail.com', "name_first":'Rayden',"name_last":'Smith', \
+         {"email":'test@gmail.com', "name_first":'Rayden', "name_last":'Smith',\
               "handle_str":'new handle'}
 
 
     # A invalid handle is given (50 characters)
-    with pytest.raises(ValueError, match = "Invalid Handle"):
+    with pytest.raises(ValueError, match="Invalid Handle"):
         funcs.user_profile_sethandle(token, create_50_string())
 
     # A invalid handle is given (2 characters)
-    with pytest.raises(ValueError, match = "Invalid Handle"):
+    with pytest.raises(ValueError, match="Invalid Handle"):
         funcs.user_profile_sethandle(token, "aa")
 
 '''
@@ -185,4 +183,3 @@ def create_50_string():
     string = "a" * 50
     assert len(string) == 50
     return string
-
