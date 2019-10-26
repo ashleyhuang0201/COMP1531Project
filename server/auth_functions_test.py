a342cac3adb51d0e1896520bf9438065e7771133
Coen Townson <coen.townson21@gmail.com>
2019-10-26 18:54:09 +1100

    auth_register works

--- server/auth_functions_test.py
+++ server/auth_functions_test.py
@@ -66,36 +66,31 @@ def test_auth_register():
     data.initialise_all()
 
     #A user is registered
-    reg_result = auth.auth_register("validcorrect@g.com", "valid_password", \
-         "valid_correct_first_name", "valid_correct_last_name")
+    user = auth.auth_register("registered@g.com", "valid_password", "a", "b")
 
     # Check database for id and token
-    user_id = get_u_id("validcorrect@g.com")
-    user_token = get_token(user_id)
+    user_id = helpers.get_user_by_email("registered@g.com").u_id
+    user_token = helpers.get_user_token_by_u_id(user_id)
 
     # confirm that register returned the correct ID and token
-    assert reg_result == {"u_id": user_id, "token": user_token}
+    assert user == {"u_id": user_id, "token": user_token}
 
     # A invalid email is given
     with pytest.raises(ValueError, match="Invalid Email"):
-        auth.auth_register("invalid_email", "valid_password", \
-             "valid_first_name", "valid_last_name")
+        auth.auth_register("invalid_email", "valid_password", "a", "b")
     # Email given is already in use
     with pytest.raises(ValueError, match="Email Already Registered"):
-        auth.auth_register("registered@g.com", "valid_password", \
-             "valid_first_name", "valid_last_name")
+        auth.auth_register("registered@g.com", "valid_password", "a", "b")
     # Password provided is not strong enough
     with pytest.raises(ValueError, match="Password Not Strong"):
-        auth.auth_register("validcorrect@g.com", "bpas", \
-             "valid_first_name", "valid_last_name")
+        auth.auth_register("validcorrect@g.com", "bpas", "a", "b")
     # First name is invalid
     with pytest.raises(ValueError, match="Invalid First Name"):
-        auth.auth_register("validcorrect@g.com", "valid_correct_password", \
-             "invalid_first_name", "valid_last_name")
+        invalid = "a" * 51
+        auth.auth_register("validcorrect@g.com", "valid_password", invalid, "b")
     # Last name is invalid
     with pytest.raises(ValueError, match="Invalid Last Name"):
-        auth.auth_register("validcorrect@g.com", "valid_correct_password", \
-             "valid_first_name", "invalid_last_name")
+        auth.auth_register("validcorrect@g.com", "valid_password", "a", invalid)
 
 def test_auth_passwordreset_request():
     '''
