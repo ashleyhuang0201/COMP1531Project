dcc35011290c468d9981e55397190d28a2c4e284
z5259842 <z5259842@cse.unsw.edu.au>
2019-11-08 01:18:33 +1100

    uploadphoto partially completed

--- server/user_functions.py
+++ server/user_functions.py
@@ -4,8 +4,10 @@ Team: You_Things_Can_Choose
 '''
 from server.Error import AccessError
 from server.helpers import get_user_by_u_id, get_user_by_token,\
-valid_user_id, valid_email, valid_token, get_user_by_email
+valid_user_id, valid_email, valid_token, get_user_by_email, valid_urls
 import server.global_var as global_var
+import urllib.request
+from PIL import Image
 
 def user_profile(token, u_id):
     """
@@ -26,10 +28,10 @@ def user_profile(token, u_id):
     # Get user object by u_id
     user = get_user_by_u_id(u_id)
 
-    #ADD PROFILE_IMG_URL
     user_profile_return = {"u_id": u_id, "email": user.email, \
          "name_first": user.name_first, "name_last": user.name_last, \
-              "handle_str": user.handle}
+              "handle_str": user.handle, "profile_img_url": \
+              user.profile_img_url}
 
     return user_profile_return
 
@@ -41,6 +43,8 @@ def user_profile_setname(token, name_first, name_last):
     - name_first is more than 50 characters
     - name_last is more than 50 characters
     """
+    if valid_token(token) is False:
+        raise AccessError("Invalid token")
     if len(name_first) > 50:
         raise ValueError("Name too long")
     if len(name_last) > 50:
@@ -65,6 +69,9 @@ def user_profile_setemail(token, email):
     - Email address is already being used by another user
 
     """
+    if valid_token(token) is False:
+        raise AccessError("Invalid token")
+
     user = get_user_by_email(email)
     if valid_email(email) is False:
         raise ValueError("Invalid email")
@@ -87,6 +94,8 @@ def user_profile_sethandle(token, handle_str):
     ValueError:
     - handle_str is more than 20 characters
     """
+    if valid_token(token) is False:
+        raise AccessError("Invalid token")
 
     if len(handle_str) > 20 or len(handle_str) < 3:
         raise ValueError("Invalid Handle")
@@ -99,43 +108,67 @@ def user_profile_sethandle(token, handle_str):
     user.update_handle(handle_str)
 
     return {}
-'''
+
 def user_profiles_uploadphoto(token, img_url, x_start, y_start, x_end, y_end):
-    
+    """
     Given a URL of an image on the internet, crops the image within x and
     y co-oridinates
 
     ValueError:
     - img_url returns an HTTP status other than 200 (2xx indicates success)
     - xy points are outside the dimensions of the image at the url
+    """
+    if valid_token(token) is False:
+        raise AccessError("Invalid token")
 
+    imageObject = Image.open(img_url)
 
-    # This function is a little hard to test, as we don't know how the image
-    # are to be handled
-
-    valid_urls = {"img1", "img2"}
-
+    #NEED TO DO THIS
+    '''
     if img_url in valid_urls:
         pass
     else:
         raise ValueError("HTTP status not 200")
+    '''
+
+    # Getting the width and height of image
+    width, height = imageObject.size
 
     # Check that the crop co-ordinates are valid
+    # Checking validity of x_start
+    if x_start < 0 or x_start >= width:
+        raise ValueError("x_start is invalid")
+    # Checking validity of x_end
+    if x_end <= 0 or x_end >= width:
+        raise ValueError("x_end is invalid")
+    # Checking validity of y_start
+    if y_start < 0 or y_start >= height:
+        raise ValueError("y_start is invalid")
+    # Checking validity of y_end
+    if y_end <= 0 or y_end >= height:
+        raise ValueError("y_end is invalid")
 
-    pass
-    
-    if valid_crop(x_start, y_start, x_end, y_end) == False:
-        raise ValueError("Crop values invalid")
+    # Obtaining user_id (u_id is filename)
+    user = get_user_by_token(token)
+    u_id = user["u_id"]
 
-    
-    # The user's profile picture is changed
+    # Gets image from url and saves it in images folder
+    urllib.request.urlretrieve(img_url, "server/images/" + u_id + ".jpg")
+
+    # Cropping image
+    cropped = imageObject.crop((x_start, y_start, x_end, y_end))
+    cropped.save(img_url)
+
     return {}
-'''
+
 
 def users_all(token):
     '''
     Shows all users
     '''
+    if valid_token(token) is False:
+        raise AccessError("Invalid token")
+
     all_users = []
 
     for user in global_var.data["users"]:
@@ -143,7 +176,8 @@ def users_all(token):
         #ADD PROFILE_IMG_URL
         user_profile_return = {"u_id": user.u_id, "email": user.email, \
          "name_first": user.name_first, "name_last": user.name_last, \
-              "handle_str": user.handle}
+              "handle_str": user.handle, "profile_img_url": \
+              user.profile_img_url}
         all_users.append(user_profile_return)
 
     return {"users": all_users}
