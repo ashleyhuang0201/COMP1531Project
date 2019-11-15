a63c05044d61185869e95480b641b0b67f1578a4
Kevin <kevin.chu1@student.unsw.edu.au>
2019-11-16 00:24:24 +1100

    Utilised better principles on search

--- server/search_function.py
+++ server/search_function.py
@@ -2,7 +2,7 @@
 Search functions
 search: Search for a message using a substring
 """
-import server.global_var as global_var
+import server.global_var as data
 from server.helpers import valid_token, get_user_by_token
 
 @valid_token
@@ -12,10 +12,13 @@ def search(token, query_str):
     channels that the user has joined that match the query
     """
 
-    # Searching for messages with query string
     messages = []
-    for channel in global_var.data["channels"]:
+
+    # Searching for messages with query string
+    for channel in data.data["channels"]:
+        # Checking all channels which the user has joined
         if channel.user_in_channel(get_user_by_token(token).u_id):
+            # Returning all messages in channel
             messages = messages + channel.search_message(token, query_str)
 
     return {"messages": messages}
--- server/search_function_test.py
+++ server/search_function_test.py
@@ -1,12 +1,12 @@
 """
 Tests for channel functions
-search: Invalid token, successful case (empty search, no such message,
-no message in channel, one result from one channel, two results from one
-channel, results from multiple channels)
+search: Invalid token, successful case (empty search, no such message, no
+message in channel, one result from one channel, two results from one channel,
+results from multiple channels)
 """
 import pytest
 import server.search_function as search
-from server.Error import AccessError, ValueError
+from server.Error import AccessError
 from server.message_functions import message_send
 from server.auth_functions import auth_register
 from server.channel_functions import channels_create
@@ -16,7 +16,7 @@ import server.global_var as global_var
 # Search given invalid token
 def test_search_invalid_token():
     """
-    Tests what happens if search is given an invalid token
+    Testing search with an invalid token
     """
     with pytest.raises(AccessError, match="Invalid token"):
         # Initialising
@@ -29,12 +29,13 @@ def test_search_invalid_token():
         # Creating channel
         channels_create(token, "chat", True)
 
+        # Searching using an invalid token
         search.search("invalid token", "123")
 
 # Search for all
 def test_search_all():
     """
-    Tests what happens if search is searching using an empty query string
+    Testing search with a search for all messages request
     """
     # Initialising
     global_var.initialise_all()
@@ -53,9 +54,10 @@ def test_search_all():
     message_send(token, channel_id, "342")
     message_send(token, channel_id, "499")
 
+    # Searching for all messages
     messages = search.search(token, '')
 
-    # Assert the list of messages returned was correct
+    # Checking if all messages are retrieved
     assert messages["messages"][0]["message"] == "499"
     assert messages["messages"][0]["u_id"] == user["u_id"]
     assert messages["messages"][1]["message"] == "342"
@@ -65,15 +67,14 @@ def test_search_all():
     assert messages["messages"][3]["reacts"] == \
          [{"react_id": 1, "u_ids": [], "is_this_user_reacted": False}]
 
+    # Ensuring that no messages after all messages retrieved
     with pytest.raises(IndexError, match="list index out of range"):
-        messages["messages"][4]
+        message = messages["messages"][4]
 
-    
-# Search for message that does not exist and get nothing
+# Search for message that does not exist
 def test_search_no_match():
     """
-    Tests what happens if search is given a substring with no such string in
-    the messages
+    Testing search with a search for no such message
     """
 
     # Initialising
@@ -98,7 +99,7 @@ def test_search_no_match():
 # Search for message with the channel being empty
 def test_search_empty_channel():
     """
-    Test what happens when a search attempt is done on an empty channel
+    Test search with a search on an empty channel
     """
     # Initialising
     global_var.initialise_all()
@@ -115,9 +116,9 @@ def test_search_empty_channel():
 # Search and get one message back
 def test_search_one():
     """
-    Testing successful case for search with a channel having one message
-    and returning one message
+    Testing successful case for a search for one message
     """
+
     # Initialising
     global_var.initialise_all()
 
@@ -135,11 +136,16 @@ def test_search_one():
     message_send(token, channel_id, "342")
     message_send(token, channel_id, "499")
 
-    messages = search.search(token, '99') 
+    # Searching message
+    messages = search.search(token, '99')
+
+    # Checking if message obtained
     assert messages["messages"][0]["message"] == "499"
     assert messages["messages"][0]["u_id"] == user["u_id"]
+
+    # Ensuring that no other messages are obtained
     with pytest.raises(IndexError, match="list index out of range"):
-        messages["messages"][1]
+        message = messages["messages"][1]
 
 # Search and get 2 messages from the same channel back
 def test_search_single_channel():
@@ -147,6 +153,7 @@ def test_search_single_channel():
     Testing successful case for search with a channel having two messages
     and returning two messages
     """
+
     # Initialising
     global_var.initialise_all()
 
@@ -164,13 +171,17 @@ def test_search_single_channel():
     message_send(token, channel_id, "342")
     message_send(token, channel_id, "499")
 
+    # Search
     messages = search.search(token, '21')
 
+    # Checking if messages are obtained
     assert messages["messages"][0]["message"] == "321"
     assert messages["messages"][0]["u_id"] == user["u_id"]
     assert messages["messages"][1]["message"] == "121"
+
+    # Ensuring that no other messages are obtained
     with pytest.raises(IndexError, match="list index out of range"):
-        messages["messages"][2]
+        message = messages["messages"][2]
 
 # Search and get 2 messages from different channels back
 def test_search_multi_channel():
@@ -197,10 +208,14 @@ def test_search_multi_channel():
     message_send(token, channel1_id, "121")
     message_send(token, channel2_id, "321")
 
+    # Search
     messages = search.search(token, '21')
 
+    # Checking if messages obtained
     assert messages["messages"][0]["message"] == "121"
     assert messages["messages"][0]["u_id"] == user["u_id"]
     assert messages["messages"][1]["message"] == "321"
+
+    # Ensuring that no other messages are obtained
     with pytest.raises(IndexError, match="list index out of range"):
-        messages["messages"][2]
+        message = messages["messages"][2]
