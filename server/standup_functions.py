a48647446caf1a4ad70969266a40ae8a94179811
Michael <z5266084@unsw.edu.au>
2019-11-16 17:01:10 +1100

    Improved standup functions

--- server/standup_functions.py
+++ server/standup_functions.py
@@ -7,17 +7,18 @@ from server.Error import AccessError, ValueError
 import server.global_var as data
 from server.helpers import get_channel_by_channel_id, get_user_by_token, \
      valid_token
+from server.constants import MAX_MESSAGE_LENGTH
 
 @valid_token
 def standup_start(token, channel_id, length):
-    """
+    '''
     For a given channel, start the standup period whereby for the next "length"
     seconds if someone calls "standup_send" with a message, it is buffered
     during the X second window then at the end of the X second window a message
     will be added to the message queue in the channel from the user who started
     the standup. X is an integer that denotes the number of seconds that the
     standup occurs for
-    """
+    '''
 
     channel = get_channel_by_channel_id(channel_id)
     user = get_user_by_token(token)
@@ -38,17 +39,17 @@ def standup_start(token, channel_id, length):
 
 @valid_token
 def standup_send(token, channel_id, message):
-    """
+    '''
     Sending a message to get buffered in the standup queue, assuming a standup
     is currently active
-    """
+    '''
 
     channel = get_channel_by_channel_id(channel_id)
     user = get_user_by_token(token)
 
     if channel is None:
         raise ValueError("Channel Does Not Exist")
-    if len(message) > 1000:
+    if len(message) > MAX_MESSAGE_LENGTH:
         raise ValueError("Message Too Long")
     if not channel.is_member(user.u_id):
         raise AccessError("Cannot Access Channel")
@@ -73,13 +74,13 @@ def standup_active(token, channel_id):
     if channel is None:
         raise ValueError("Channel Does Not Exist")
 
-    if channel.in_standup is not False:
-        # If channel is active
+    # If standup is active
+    if channel.in_standup:
         return {
             "is_active": True,
             "time_finish": channel.time_left_standup()
         }
-    # Channel is not active
+    # Standup is not active
     return {
         "is_active": False,
         "time_finish": None
