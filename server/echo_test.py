16385326e088f9b3608f08fdfd84a77e9e6ebd93
Kevin <kevin.chu1@student.unsw.edu.au>
2019-11-17 17:45:41 +1100

    Finalising echo_test

--- server/echo_test.py
+++ server/echo_test.py
@@ -1,6 +1,14 @@
+'''
+Team: You_Things_Can_Choose
+Tests echo
+'''
+
 import server.echo as echo
 
 def test_echo():
+    '''
+    Tests echo: Single character, multiple characters
+    '''
     assert echo.echo("1") == "1", "1 == 1"
     assert echo.echo("abc") == "abc", "abc == abc"
     assert echo.echo("trump") == "trump", "trump == trump"
