9d7e2f6db7bdcd1420326a4baace2a4166eed47e
z5259842 <z5259842@cse.unsw.edu.au>
2019-11-17 17:23:49 +1100

    unnecessary else

--- server/helpers.py
+++ server/helpers.py
@@ -259,13 +259,12 @@ def to_bool(val):
     ''' Typecasting to bool '''
     if val is None:
         raise ValueError("Value was missing - please check you input")
-    
+
     print(f"{val} is {type(val)}")
 
     if val == 'false':
         return False
-    else:
-        return True
+    return True
 
 def to_float(val):
     ''' Typecasting to float'''
