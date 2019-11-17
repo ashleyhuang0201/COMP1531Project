f0502e57c91e5abae65372d2d42199cabcb86690
Coen Townson <coen.townson21@gmail.com>
2019-11-17 15:02:56 +1100

    Updated flask server to run in dev mode finally - why didnt we do this earlier

--- server.py
+++ server.py
@@ -511,4 +511,4 @@ def get_image(path):
 
 
 if __name__ == '__main__':
-    APP.run(port=(sys.argv[1] if len(sys.argv) > 1 else 5001))
+    APP.run(port=(sys.argv[1] if len(sys.argv) > 1 else 5001), debug=True)
